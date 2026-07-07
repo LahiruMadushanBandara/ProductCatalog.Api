6c39c2a1fe69f92cee9100c46a2ac05d700627a6
lahiru <lahiru@theavo.net>
2026-07-07 23:11:31 +0530

    enable swagger ui and connection string injected to repository in configurations

--- ProductCatalog.Api/Program.cs
+++ ProductCatalog.Api/Program.cs
@@ -1,3 +1,4 @@
+using ProductCatalog.Api.Integrations;
 using ProductCatalog.Api.Middlewares;
 using ProductCatalog.Api.Repositories;
 using ProductCatalog.Api.Services;
@@ -11,12 +12,36 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddScoped<IProductRepository>(sp =>
+{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+
+    var connectionString = configuration.GetConnectionString("Default")
+        ?? throw new InvalidOperationException("Connection string was not found.");
+
+    return new ProductRepository(connectionString);
+});
+
+
+builder.Services.AddHttpClient<IProductApiClient, ProductApiClient>(client =>
+{
+    client.BaseAddress = new Uri("https://dummyjson.com/");
+    client.Timeout = TimeSpan.FromSeconds(10);
+});
+
+
 
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 
