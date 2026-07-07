6a296264b0c29843156c74919f12f4933f17818a
lahiru <lahiru@theavo.net>
2026-07-07 19:55:34 +0530

    product model

---
+++ ProductCatalog.Api/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductCatalog.Api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string? Category { get; set; }
+        public string? Brand { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
